681e8ee394c8a469dbe191e8e8709a24b3b72dd3
Sarah Romanes <you@example.com>
2018-10-02 23:41:29 +1000

    knn fix

--- shiny/LinearRegression/app.R
+++ shiny/LinearRegression/app.R
@@ -32,18 +32,22 @@ ui <- fluidPage(
                      withMathJax(helpText("The value of the intercept, $$\\beta_0$$")),
                      min = -50,
                      max = -30,
-                     value = -40),
+                     value = -39.5,
+                     step=0.5),
          sliderInput("slope",
                      withMathJax(helpText("The value of the slope, $$\\beta_1$$")),
                      min = 1,
                      max = 10,
-                     value = 6)
+                     value = 5.5,
+                     step=0.5)
          
          
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
+         textOutput("RSS"),
+         br(),
          plotOutput("lmPlot")
       )
    )
@@ -56,16 +60,35 @@ server <- function(input, output) {
       # generate bins based on input$bins from ui.R
      coef <- c(input$slope, input$intercept)
      Fitted <- coef[2] + coef[1]*data$Education
-     Income <- transform(data, Fitted = Fitted)
+   
+     Income <- transform(data, Fitted = Fitted, Resid = Income-Fitted)
+     Pos <- Income[which(Income$Resid>0),]
+     Neg <- Income[which(Income$Resid<0),]
+     
+     Pos <- transform(Pos, xr = Education- Resid)
+     Neg <- transform(Neg, xr = Education +abs(Resid))
      
       
       # draw the histogram with the specified number of bins
-     ggplot(Income, aes(Education, Income)) + 
-       geom_point(color="red") + 
-       geom_abline(slope=coef[1], intercept=coef[2], col="dodgerblue") +
+     p <- ggplot(Income, aes(Education, Income)) + 
+       geom_point(color="black") + 
+       geom_abline(slope=coef[1], intercept=coef[2], col="darkorchid4") +
        geom_segment(aes(x = Education, y = Income,
                         xend = Education, yend = Fitted)) +
-       coord_cartesian(xlim = c(9, 23), ylim = c(15, 90))
+       geom_rect(data=Neg, aes(xmin=Education, xmax=xr, ymax=Fitted, ymin=Income), color="darkorchid1", fill="darkorchid1", alpha=0.3)+
+       geom_rect(data=Pos, aes(xmin=xr, xmax=Education, ymax=Income, ymin=Fitted), color="darkorchid1", fill="darkorchid1", alpha=0.3)+
+       coord_equal(ratio=1, ylim=c(15, 100), xlim=c(-10, 30))
+     
+     p + theme(axis.text = element_text(size = 20)) +theme_minimal()
+   }, height=600, width=400)
+   
+   output$RSS <- renderText({
+     coef <- c(input$slope, input$intercept)
+     Fitted <- coef[2] + coef[1]*data$Education
+     
+     Income <- transform(data, Fitted = Fitted, Resid = Income-Fitted)
+     rss <- round(sum((Income$Resid)^2),2)
+     paste0("RSS = ", rss, ". This is the value of the sum of the squares on the plot, of which we are trying to minimise.")
      
    })
 }
